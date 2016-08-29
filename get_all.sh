8263a94df962434494c769b30771306fc0145c3f
Valentin Puente <vpuente@unican.es>
2016-08-29 11:56:39 +0200

    Reference in separate dir

--- get_all.sh
+++ get_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget http://www.cs.wisc.edu/~remzi/OSTEP/Homework/all.tgz 
-mkdir HW; cd HW
+mkdir Reference; cd Reference 
 tar xf ../all.tgz
 find . -type d | xargs -L1 -I{}  sh -c 'cd {}; tar xf *.tgz; rm *.tgz' 
 cd ../; rm all.tgz
