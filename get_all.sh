aa29f7a0ac8395dec0737a5c903245c1994c0019
Valentin Puente <vpuente@unican.es>
2016-08-29 11:57:49 +0200

    Add source html

--- get_all.sh
+++ get_all.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wget http://www.cs.wisc.edu/~remzi/OSTEP/Homework/all.tgz 
 mkdir Reference; cd Reference 
+wget http://pages.cs.wisc.edu/~remzi/OSTEP/Homework/homework.html
 tar xf ../all.tgz
 find . -type d | xargs -L1 -I{}  sh -c 'cd {}; tar xf *.tgz; rm *.tgz' 
 cd ../; rm all.tgz
