c4719150e8f6068309093fe13989a649598138f1
Zdravko Dimitrov <zdravko@MacBookPro-Zdravko.local>
2021-09-09 01:03:39 +0200

    Comienzo de memoria y fastsort iniciado.

---
+++ Projects/P0/fastsort.c
@@ -0,0 +1,26 @@
+#include <stdio.h> //fopen(), fgets(), fclose(), qsort()
+#include <stdlib.h> //atoi(), 
+#include <sys/stat.h> //stat() y fstat()
+#include <string.h> //strcmp(), strtok()
+
+int main (int argc, char *argv[]){
+
+/*PSEUDOCÓDIGO INCREMENTAL
+    1.Recoger los parámetros
+    2.Leer las líneas del fichero
+    3.Mostrarlas por pantalla
+
+PSEUDOCÓDIGO FASTSORT
+    1.Recoger los parámetros
+    2.Averiguar las dimensiones del fichero
+    3.Leer el texto
+    4.Separar adecuadamente
+    5.Ordenar adecuadamente
+
+
+*/
+
+
+
+ return 0;
+}
